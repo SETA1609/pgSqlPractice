5ac9b90ff04c95d060a0171def6852ed5225fe92
seta1609 <sebastian.stp16@gmail.com>
2024-02-04 23:43:39 +0100

    schema 5 and cli6

---
+++ michigan_pgsql/course_2/schemas/schema5.sql
@@ -0,0 +1,37 @@
+-- After running these schema the previews exercise with tracks would be dropped
+DROP TABLE album CASCADE;
+CREATE TABLE album (
+    id SERIAL,
+    title VARCHAR(128) UNIQUE,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE track CASCADE;
+CREATE TABLE track (
+    id SERIAL,
+    title TEXT, 
+    artist TEXT, 
+    album TEXT, 
+    album_id INTEGER REFERENCES album(id) ON DELETE CASCADE,
+    count INTEGER, 
+    rating INTEGER, 
+    len INTEGER,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE artist CASCADE;
+CREATE TABLE artist (
+    id SERIAL,
+    name VARCHAR(128) UNIQUE,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE tracktoartist CASCADE;
+CREATE TABLE tracktoartist (
+    id SERIAL,
+    track TEXT,
+    track_id INTEGER REFERENCES track(id) ON DELETE CASCADE,
+    artist TEXT,
+    artist_id INTEGER REFERENCES artist(id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
